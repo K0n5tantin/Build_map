f71296e35a417b210c41e93cc0450d6742124a0d
TEAM\Trishkin <Trishkin@competentum.ru>
2019-08-23 18:19:52 +0300

    [TestFront End]

--- TestFrotEnd/js/balls.js
+++ TestFrotEnd/js/balls.js
@@ -1,14 +1,14 @@
 function animate(options) {
 
-  var start = performance.now();
+  let start = performance.now();
 
   requestAnimationFrame(function animate(time) {
     // timeFraction от 0 до 1
-    var timeFraction = (time - start) / options.duration;
+    let timeFraction = (time - start) / options.duration;
     if (timeFraction > 1) timeFraction = 1;
 
     // текущее состояние анимации
-    var progress = options.timing(timeFraction)
+    let progress = options.timing(timeFraction)
     
     options.draw(progress);
 
@@ -19,46 +19,23 @@ function animate(options) {
   });
 }
 
- function makeEaseOut(timing) {
-      return function(timeFraction) {
-        return 1 - timing(1 - timeFraction);
-      }
+function makeEaseOut(timing) {
+  return function(timeFraction) {
+    return 1 - timing(1 - timeFraction);
+  }
+}
+
+function bounce(timeFraction) {
+  for (let a = 0, b = 1, result; 1; a += b, b /= 2) {
+    if (timeFraction >= (7 - 4 * a) / 11) {
+      return -Math.pow((11 - 6 * a - 11 * timeFraction) / 4, 2) + Math.pow(b, 2)
     }
+  }
+}
 
-    function bounce(timeFraction) {
-      for (let a = 0, b = 1, result; 1; a += b, b /= 2) {
-        if (timeFraction >= (7 - 4 * a) / 11) {
-          return -Math.pow((11 - 6 * a - 11 * timeFraction) / 4, 2) + Math.pow(b, 2)
-        }
-      }
-    }
-
-    function quad(timeFraction) {
-      return Math.pow(timeFraction, 2);
-    }
-
-    ball.onclick = function() {
-
-      let height = 800 - ball.clientHeight;
-      let width = 100;
-
-      animate({
-        duration: 2000,
-        timing: makeEaseOut(bounce),
-        draw: function(progress) {
-          ball.style.top = height * progress + 'px'
-        }
-      });
-
-      animate({
-        duration: 2000,
-        timing: makeEaseOut(quad),
-        draw: function(progress) {
-          ball.style.left = width * progress + "px"
-        }
-      });
-    }
-
+function quad(timeFraction) {
+  return Math.pow(timeFraction, 2);
+}
 
 function dragstart_handler(ev) {
  console.log("dragStart");
@@ -72,7 +49,7 @@ function dragstart_handler(ev) {
  
  /*
  console.log("dragBox", ev);
- var dragBox = ev.target.getBoundingClientRect();
+ let dragBox = ev.target.getBoundingClientRect();
  console.log("dragBox", dragBox, ev.screenX, ev.screenY, ev.clientX, ev.clientY);
   console.log("dragBox-x", ev.screenX - ev.target.getBoundingClientRect().x);
   console.log("dragBox-y", ev.screenY - ev.target.getBoundingClientRect().y);
@@ -88,8 +65,8 @@ function dragover_handler(ev) {
  
  ev.preventDefault();
  /*
- var id = ev.dataTransfer.getData("text");
- var el = document.getElementById(id);
+ let id = ev.dataTransfer.getData("text");
+ let el = document.getElementById(id);
  console.log("element", el);
  */
  //console.log("element", el.getBoundingClientRect().x, el.getBoundingClientRect().y);
@@ -101,13 +78,13 @@ function drop_handler(ev) {
   console.log(ev);
   // Get the id of drag source element (that was added to the drag data
   // payload by the dragstart event handler)
-  var id = ev.dataTransfer.getData("text");
-  var el = document.getElementById(id);
+  let id = ev.dataTransfer.getData("text");
+  let el = document.getElementById(id);
   
   // Only Move the element if the source and destination ids are both "move"
   if (id == "src_move" && ev.target.id == "dest_move"){
     /*
-    var dropPlace = {
+    let dropPlace = {
       x: el 
     }
     */
@@ -124,20 +101,41 @@ function drop_handler(ev) {
     el.style.top = (ev.screenY - ev.target.getClientRects().y) + "px";
     */
     
-    var dropBox = ev.target.getBoundingClientRect();
+    let dropBox = ev.target.getBoundingClientRect();
     
     //console.log(ev.clientX, dropBox.x, ev.screenX - dropBox.x);
     //console.log(ev.clientY, dropBox.y, ev.screenY - dropBox.y);
     
-    el.style.left = (ev.clientX - dropBox.x - 0.5*el.getBoundingClientRect().width) + "px";
-    el.style.top = (ev.clientY - dropBox.y - 0.5*el.getBoundingClientRect().height) + "px";
+    let dropLeft = (ev.clientX - dropBox.x - 0.5*el.getBoundingClientRect().width);
+    let dropTop = (ev.clientY - dropBox.y - 0.5*el.getBoundingClientRect().height);
+    
+    //--------------------
+    let height = 800 - dropTop - el.clientHeight;
+    let width = 100;
+
+    animate({
+      duration: 2000,
+      timing: makeEaseOut(bounce),
+      draw: function(progress) {
+        el.style.top = dropTop + height * progress + 'px'
+      }
+    });
+
+    animate({
+      duration: 2000,
+      timing: makeEaseOut(quad),
+      draw: function(progress) {
+        el.style.left = dropLeft + width * progress + "px"
+      }
+    });
+    //---------------------------
     
     console.log(dropBox);
   }
   
   // Copy the element if the source and destination ids are both "copy"
   if (id == "src_copy" && ev.target.id == "dest_copy") {
-   var nodeCopy = document.getElementById(id).cloneNode(true);
+   let nodeCopy = document.getElementById(id).cloneNode(true);
    nodeCopy.id = "newId";
    ev.target.appendChild(nodeCopy);
   }
@@ -155,14 +153,14 @@ window.onload = function(){
 	
   
   
-  var elements = document.querySelectorAll(".graphs");
+  let elements = document.querySelectorAll(".graphs");
   
-  for (var i = 0; i < elements.length; i++) {
+  for (let i = 0; i < elements.length; i++) {
     elements[i].addEventListener('click', event => displayCanvas());
   }
   
-  var input_boxes = document.querySelectorAll(".input_box");
-  for (var i = 0; i < input_boxes.length; i++) {
+  let input_boxes = document.querySelectorAll(".input_box");
+  for (let i = 0; i < input_boxes.length; i++) {
     input_boxes[i].addEventListener('click', event => displayCanvas());
     input_boxes[i].addEventListener('keyup', event => displayCanvas());
     
